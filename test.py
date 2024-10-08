ebd1d3905103010390e59d1b6e678f1c5ea3a03b
Татьяна <89996807+PodolskayaTatiana@users.noreply.github.com>
2024-10-08 13:58:15 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,81 @@
+import pytest
+from suitable_superheroes import get_superhero
+
+def test_male_gender_without_profession():
+    res = get_superhero("Male", False)
+    assert not res == False
+    gender = res["appearance"]["gender"]
+    assert gender == "Male"
+
+def test_female_gender_without_profession():
+    res = get_superhero("Female", False)
+    assert not res == False
+    gender = res["appearance"]["gender"]
+    assert gender == "Female"
+
+def test_entering_a_non_gender_without_profession():
+    res = get_superhero("xxx", False)
+    assert not res == True
+
+def lowercase_letters_of_female_gender():
+    res = get_superhero("female", False)
+    assert not res == True
+#???????????????????????????????????????????
+def lowercase_letters_of_female_gender():
+    res = get_superhero("male", False)
+    assert not res == True
+
+def test_entering_integer_gender():    
+    res = get_superhero(1, False)
+    assert not res == True
+
+def test_there_is_a_job_for_the_male_gender():
+    res = get_superhero("Male", True)
+    work = res["work"]["occupation"]
+    assert work != "-"
+
+def test_there_is_no_work_for_the_male_gender():
+    res = get_superhero("Male", False)
+    work = res["work"]["occupation"]
+    assert work == "-"
+
+def test_entering_a_non_work():
+    res = get_superhero("Male", 2)
+    work = res["work"]["occupation"]
+    assert work != "-"
+    #?????????????????????????????
+
+def test_the_height_of_a_man_without_a_job():
+    res = get_superhero("Male", False)
+    height = res["appearance"]["height"][1]
+    assert not height == False
+
+def test_the_height_of_a_man_with_a_job():
+    res = get_superhero("Male", True)
+    height = res["appearance"]["height"][1]
+    assert not height == False
+
+def test_the_height_of_a_woman_without_a_job():
+    res = get_superhero("Female", False)
+    height = res["appearance"]["height"][1]
+    assert not height == False
+
+def test_the_height_of_a_woman_with_a_job():
+    res = get_superhero("Female", True)
+    height = res["appearance"]["height"][1]
+    assert not height == False
+
+
+#def test_invalid_inputs():
+#    assert get_superhero("male", "") == False
+#    assert get_superhero("female", "") == False
+#    assert get_superhero("", "") == False
+#    assert get_superhero("123", "") == False
+
+
+
+    # input_validator.py
+
+
+    # input_validator.py
+
